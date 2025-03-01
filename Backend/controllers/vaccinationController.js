695027512067cdc8ca225902b3027e2e5531b4ac
dev-code-create <ayushkankale04@gmail.com>
2025-03-01 14:58:20 +0530

    Update vaccinationController.js

--- Backend/controllers/vaccinationController.js
+++ Backend/controllers/vaccinationController.js
@@ -14,12 +14,12 @@ export const createVaccination = async (req, res) => {
 
     //send reminder email vaccine
     const pet = await vaccination.populate("pet");
-    sendVaccinationReminder(
-      pet.owner.email,
-      pet.name,
-      vaccineType,
-      nextDueDate
-    );
+    // sendVaccinationReminder(
+    //   pet.owner.email,
+    //   pet.name,
+    //   vaccineType,
+    //   nextDueDate
+    // );
     res.status(400).json(vaccination);
   } catch (error) {
     res.status(500).json({ message: error.message });
