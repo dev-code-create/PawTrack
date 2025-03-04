7458c1f0b36b4433746a176a0b7ca0a4342ebdc1
dev-code-create <ayushkankale04@gmail.com>
2025-03-04 18:20:58 +0530

    App.jsx modify

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,11 +1,23 @@
-import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { AnimatePresence } from "framer-motion";
 import HomePage from "./pages/HomePage";
+import SignInPage from "./pages/SignInPage";
+
+// import Dashboard from "./pages/Dashboard";
+import SignUpPage from "./pages/SignOutPage";
 
 const App = () => {
   return (
-    <div>
-      <HomePage />
-    </div>
+    <Router>
+      <AnimatePresence mode="wait">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/sign-in" element={<SignInPage />} />
+          <Route path="/sign-up" element={<SignUpPage />} />
+          {/* <Route path="/dashboard" element={<Dashboard />} /> */}
+        </Routes>
+      </AnimatePresence>
+    </Router>
   );
 };
 
