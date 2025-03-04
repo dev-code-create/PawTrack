4b71b7293192c3e665a70761a4fe32e40c29cd69
dev-code-create <ayushkankale04@gmail.com>
2025-03-05 00:21:19 +0530

    Theme Toggle done

--- Frontend/src/pages/ThemeToggle.jsx
+++ Frontend/src/pages/ThemeToggle.jsx
@@ -1,2 +1,16 @@
-const ThemeToggle = () => {};
+import { Button } from "@/components/ui/button";
+import { useTheme } from "@/lib/ThemeProvider";
+import { Moon, Sun } from "lucide-react";
+
+const ThemeToggle = () => {
+  const { theme, toggleTheme } = useTheme();
+  return (
+    <Button
+      onClick={toggleTheme}
+      className="fixed bottom-4 right-4 p-2 bg-white dark:bg-gray-800 rounded-full shadow-md"
+    >
+      {theme === "dark" ? <Sun /> : <Moon />}
+    </Button>
+  );
+};
 export default ThemeToggle;
